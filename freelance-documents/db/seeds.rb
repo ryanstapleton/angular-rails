edf7fba868338e789dac14292c39cb5ef642e237
ryanstapleton <megalixir.1@gmail.com>
2018-03-27 14:05:56 -0600

    Created rails API for documents, added seed data

--- freelance-documents/db/seeds.rb
+++ freelance-documents/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#   t.string "title"
+#   t.string "description"
+#   t.text "file_url"
+#   t.text "image_url"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+
+10.times do |d|
+  FreelanceDocument.create!(
+    title: "Document #{d + 1}", 
+    description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem   aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    file_url: "https://drive.google.com/open?id=1jVevRIKLoS2eXWthsd9mxVT9NFKYWXsfvS0vHFyHxRA",
+    image_url: "https://membermouse.com/wp-content/uploads/2016/09/Freelancer-Liability-Featured.png"
+  )
+end
